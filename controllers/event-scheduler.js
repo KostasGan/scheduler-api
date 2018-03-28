fbb7abb43bccf5d94b2e2a6d9b7c0c57c1590d2e
KostasGan <kostasgan@e-food.gr>
2018-03-28 17:06:04 +0300

    refactor for better use

--- controllers/event-scheduler.js
+++ controllers/event-scheduler.js
@@ -3,43 +3,50 @@ const Promise = require('bluebird');
 const moment = require('moment');
 
 let calendar = google.calendar('v3');
-let unAuthUsers = [];
 
 exports.GetCalendarEvents = (oauth2Client) => {
-    calendar.events.list({
-        auth: oauth2Client,
-        calendarId: 'primary',
-        timeMin: (new Date(2018,02,01,14)).toISOString(),
-        timeMax: (new Date(2018,02,01, 18)).toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: 'startTime'
-    }, function (err, response) {
-        if (err) {
-            console.log('The API returned an error: ' + err);
-            return new Promise((resolve, reject) => { 
-                reject({
-                    "message": err
+    return new Promise((resolve, reject) =>{
+        calendar.events.list({
+            auth: oauth2Client,
+            calendarId: 'primary',
+            timeMin: (new Date(2018,02,01,10)).toISOString(),
+            timeMax: (new Date(2018,02,28,18)).toISOString(),
+            maxResults: 10,
+            singleEvents: true,
+            orderBy: 'startTime'
+        }, function (err, response) {
+            if (err) {
+                console.log('The API returned an error: ' + err.message);
+                return reject({
+                    "status": "error",
+                    "message": err.message
                 });
-            });
-        }
-        var events = response.items;
-        if (events.length == 0) {
-            console.log('No upcoming events found.');
-            return new Promise((resolve, reject) => { resolve([])});
-        } else {
-            new Promise.map(events, (event) => {
-                return {
-                    event_id: event.id,
-                    summary: event.summary,
-                    startDate: new Date(event.start.dateTime),
-                    startHour: new Date(event.start.dateTime).getHours(),
-                    endDate: new Date(event.end.dateTime),
-                    endDate: new Date(event.end.dateTime).getHours()
-                };
-            }).then((some) =>{
-                console.log(some);
-            });
-        }
+            }
+
+            let events = response.items || [];
+
+            if (events.length == 0) {
+                return resolve({
+                    "status": "success",
+                    "events": []
+                });
+            } else {
+                return new Promise.map(events, (event) => {
+                    return {
+                        event_id: event.id,
+                        summary: event.summary,
+                        startDate: new Date(event.start.dateTime),
+                        startHour: new Date(event.start.dateTime).getHours(),
+                        endDate: new Date(event.end.dateTime),
+                        endHour: new Date(event.end.dateTime).getHours()
+                    };
+                }).then((new_events) => {
+                    return resolve({
+                        "status": "success",
+                        "events": new_events
+                    });
+                });
+            }
+        });
     });
 }
--- routes/default.js
+++ routes/default.js
@@ -2,6 +2,7 @@ const googleAuth = require('google-auth-library');
 const bodyParser = require('body-parser');
 const auth = require('../helpers/oauth');
 const ev_controler = require('../controllers/event-scheduler');
+const userModel = require('../models/user').Model;
 
 function initGoogleAuth(credentials, ac_token){
     let auth = new googleAuth();
@@ -16,13 +17,22 @@ exports.registerRoutes = function(app, config) {
     let access_token;
     let credentials = config.get('web');
     
+    let unAuthUsers = [];
+
     app.post('/', (req, res) => {
         // let startDate = req.body.startDate || '';
         // let endDate = req.body.endDate || '';
         // let availableTime = req.body.availableTime || '';
         // let friends_list = req.body.friends_list || '';
+        let friends_list = 'kostas.efood@gmail.com,doctorkos69@gmail.com'
 
-        access_token = req.body.access_token.trim() || '';
+        // let friends = userModel.findFriendsAccessToken(friends_list);
+        
+        // friends.then((si) => {
+        //     console.log(si);
+        // });
+
+        access_token = req.body.access_token || '';
         
         if(access_token === ''){
             res.json({message: 'No access token. Please try again!'});
@@ -31,6 +41,9 @@ exports.registerRoutes = function(app, config) {
         let oauth2Client = initGoogleAuth(credentials, access_token);
         ev_controler.GetCalendarEvents(oauth2Client).then((val) =>{
             res.json(val);
+        }).catch((error) => {
+            res.status(401);
+            res.json(error);
         });
     });
 
