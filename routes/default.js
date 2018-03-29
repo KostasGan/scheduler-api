40e9581942734949540a53650b4f9a1084e781b2
Kostas Gan <kostasgan@e-food.gr>
2018-03-30 01:52:28 +0300

    get friendlist & access_tokens are valid functionality

--- routes/default.js
+++ routes/default.js
@@ -1,14 +1,15 @@
 const googleAuth = require('google-auth-library');
 const bodyParser = require('body-parser');
+const Promise = require('bluebird');
 const auth = require('../helpers/oauth');
 const ev_controler = require('../controllers/event-scheduler');
 const userModel = require('../models/user').Model;
 
-function initGoogleAuth(credentials, ac_token){
+function initGoogleAuth(credentials){
     let auth = new googleAuth();
 
     let googleOauth2Client = new auth.OAuth2(credentials.client_id, credentials.client_secret, credentials.redirect_uris);
-    googleOauth2Client.credentials = {"access_token": ac_token};
+    // googleOauth2Client.credentials = {"access_token": ac_token};
 
     return googleOauth2Client;
 }
@@ -17,34 +18,60 @@ exports.registerRoutes = function(app, config) {
     let access_token;
     let credentials = config.get('web');
     
-    let unAuthUsers = [];
-
     app.post('/', (req, res) => {
         // let startDate = req.body.startDate || '';
         // let endDate = req.body.endDate || '';
         // let availableTime = req.body.availableTime || '';
         // let friends_list = req.body.friends_list || '';
         let friends_list = 'kostas.efood@gmail.com,doctorkos69@gmail.com'
-
-        // let friends = userModel.findFriendsAccessToken(friends_list);
-        
-        // friends.then((si) => {
-        //     console.log(si);
-        // });
-
         access_token = req.body.access_token || '';
-        
+
         if(access_token === ''){
             res.json({message: 'No access token. Please try again!'});
             return; 
         }
-        let oauth2Client = initGoogleAuth(credentials, access_token);
-        ev_controler.GetCalendarEvents(oauth2Client).then((val) =>{
-            res.json(val);
-        }).catch((error) => {
-            res.status(401);
-            res.json(error);
+
+        let oauth2Client = initGoogleAuth(credentials);
+
+        userModel.findFriendsAccessToken(friends_list).then((friends) => {
+            new Promise.map(friends, (friend) => {
+                oauth2Client.credentials = {"access_token": friend.ac_token};
+
+                return auth.checkAuthToken(oauth2Client).then((val) =>{
+                    if(val.message === "Invalid Credentials"){
+                        return friend.email;
+                    }
+                });
+            }).then((unAuthUsers) =>{
+                return {
+                    friend: friends,
+                    unAuthUsers: unAuthUsers
+                }
+            }).then((obj) => {
+                //console.log(obj);
+                if (obj.unAuthUsers.length >= 1){
+                    console.log(obj.unAuthUsers);
+                    res.status(401);
+                    res.json({
+                        status: "error",
+                        error_code: "unauthorized_friends",
+                        message: "Some of friendList's users need Authorization",
+                        data : {
+                            users:obj.unAuthUsers
+                        }
+                    });
+                    return;
+                }
+            });
         });
+
+        // let oauth2Client = initGoogleAuth(credentials, access_token);
+        // ev_controler.GetCalendarEvents(oauth2Client).then((val) =>{
+        //     res.json(val);
+        // }).catch((error) => {
+        //     res.status(401);
+        //     res.json(error);
+        // });
     });
 
     app.post('/gauthredirect', (req, res) => {
@@ -58,6 +85,9 @@ exports.registerRoutes = function(app, config) {
         let oauth2Client = initGoogleAuth(credentials, access_token);
         auth.authorizeClient(oauth2Client).then((val) =>{
             res.json(val);
+        }).catch((error) => {
+            res.status(401);
+            res.json(error);
         });
     });
 }
