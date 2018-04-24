3d13321a80f0eae5c4f0ee61413deeb761e7b70e
Kostas Gan <kostasgan@e-food.gr>
2018-04-24 23:44:37 +0300

    work in progress

--- routes/default.js
+++ routes/default.js
@@ -23,7 +23,7 @@ exports.registerRoutes = function(app, config) {
         // let endDate = req.body.endDate || '';
         // let availableTime = req.body.availableTime || '';
         // let friends_list = req.body.friends_list || '';
-        let friends_list = 'kostas.efood1@gmail.com'
+        let friends_list = 'kostasgan@e-food.gr'
         access_token = req.body.access_token || '';
 
         if(access_token === ''){
@@ -33,50 +33,99 @@ exports.registerRoutes = function(app, config) {
 
         let oauth2Client = initGoogleAuth(credentials);
 
-        userModel.findFriendsAccessToken(friends_list).then((friends) => {
-            return Promise.filter(friends, (friend) => {
-                oauth2Client.credentials = {"access_token": friend.ac_token};
+        let friends = userModel.findFriendsAccessToken(friends_list);
+        let main_user = userModel.findFriendsAccessToken('kostasgan@e-food.gr');
 
-                return auth.checkAuthToken(oauth2Client).then((val) =>{
-                    if(val.message === "Invalid Credentials"){
-                        return friend.email;
-                    }
-                    return;
-                });
-            }).then((unAuthUsers) =>{
-                return Promise.props({
-                    friend: friends,
-                    unAuthUsers: Promise.map(unAuthUsers, (us) => {return us.email})
-                });
+        friends.filter((friend) => {
+            oauth2Client.credentials = {"access_token": friend.ac_token};
+
+            return auth.checkAuthToken(oauth2Client).then((val) =>{
+                if(val.message === "Invalid Credentials"){
+                    return friend.email;
+                }
+                return;
             });
-        }).then((obj) => {
-            if (obj.unAuthUsers.length >= 1){
-                console.log(obj.unAuthUsers);
+        }).map((friend) => {
+            return friend.email;
+        }).then((unAuthUsers) => {
+            if(unAuthUsers.length > 0){
+                console.log(unAuthUsers);
                 res.status(401);
                 res.json({
                     status: "error",
                     error_code: "unauthorized_friends",
                     message: "Some of friendList's users need Authorization",
                     data : {
-                        users:obj.unAuthUsers
+                        users: unAuthUsers
                     }
                 });
                 return;
             }
-            else{
-                let some = [];
-                oauth2Client.credentials = {"access_token": access_token};
-                ev_controler.GetCalendarEvents(oauth2Client).then((val) =>{
-                    some.push(val);
-                }).catch((e) => {
-                    console.log(e);
-                });
-                Promise.each(obj.friend, (us) => {
-                    oauth2Client.credentials = {"access_token": us.ac_token};
-                    ev_controler.GetCalendarEvents(oauth2Client).then((es) => { some.push(es);})
-                });
-            }
+
+            return Promise.props({
+                main_user: main_user,        
+                friends: friends
+            });
+        }).then((prop) => {
+            let list = [];
+            list.push(prop.main_user.ac_token);
+            prop.friends.each((friend) => {
+                list.push(friend.ac_token);
+            });
+
+            list.map((items) => {
+                oauth2Client.credentials = {"access_token": items.ac_token};
+
+                return ev_controler.GetCalendarEvents(oauth2Client);
+            }).then((sl)=> {console.log(sl);});
+
         });
+
+        // userModel.findFriendsAccessToken(friends_list).then((friends) => {
+        //     return Promise.filter(friends, (friend) => {
+        //         oauth2Client.credentials = {"access_token": friend.ac_token};
+
+        //         return auth.checkAuthToken(oauth2Client).then((val) =>{
+        //             if(val.message === "Invalid Credentials"){
+        //                 return friend.email;
+        //             }
+        //             return;
+        //         });
+        //     }).then((unAuthUsers) =>{
+        //         return Promise.props({        
+        //             friend: friends,
+        //             unAuthUsers: Promise.map(unAuthUsers, (us) => {return us.email})
+        //         });
+        //     });
+        // }).then((obj) => {
+        //     if (obj.unAuthUsers.length >= 1){
+        //         console.log(obj.unAuthUsers);
+        //         res.status(401);
+        //         res.json({
+        //             status: "error",
+        //             error_code: "unauthorized_friends",
+        //             message: "Some of friendList's users need Authorization",
+        //             data : {
+        //                 users:obj.unAuthUsers
+        //             }
+        //         });
+        //         return;
+        //     }
+        //     else{
+        //         let some = [];
+        //         oauth2Client.credentials = {"access_token": access_token};
+        //         ev_controler.GetCalendarEvents(oauth2Client).then((val) =>{
+        //             console.log(val);
+        //             some.push(val);
+        //         }).catch((e) => {
+        //             console.log(e);
+        //         });
+        //         Promise.each(obj.friend, (us) => {
+        //             oauth2Client.credentials = {"access_token": us.ac_token};
+        //             ev_controler.GetCalendarEvents(oauth2Client).then((es) => { some.push(es); console.log(some);})
+        //         });
+        //     }
+        // });
     });
 
     app.post('/gauthredirect', (req, res) => {
