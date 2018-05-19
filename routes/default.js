3d2a7b0772b6b36358bb8a131f4da5cef63e37a6
Kostas Gan <kostasgan@e-food.gr>
2018-05-19 22:27:13 +0300

    fix wrong dates input

--- routes/default.js
+++ routes/default.js
@@ -43,15 +43,17 @@ function checkAvailableTime(available_time){
     });
 }
 
-function formatDateWithTime(startDate, endDate, diffDate, available_time){
+function formatDateWithTime(startDate, diffDate, available_time){
     let range = [];
+
     for(let i=0; i <= diffDate; i++){
         let av_time = available_time[i].trim().split(/[":\-"]/);
-        
+        let new_date = new moment(startDate.toISOString());
+
         if(av_time[i] !== "0" && av_time[i+1] !== "0"){
             range.push({
-                "startDate": startDate.add(i, "d").set({'hour': av_time[0], 'minutes': av_time[1]}).toISOString(),
-                "endDate": startDate.set({'hour': av_time[2], 'minutes': av_time[3]}).toISOString()
+                "startDate": new_date.add(i, "d").set({'hour': av_time[0], 'minutes': av_time[1]}).toISOString(),
+                "endDate": new_date.set({'hour': av_time[2], 'minutes': av_time[3]}).toISOString()
             });
         }
     }
@@ -159,9 +161,9 @@ exports.registerRoutes = function(app, config) {
                                 return;
                             }
                             else if(events.length > 0){
-                                let currentStartHour = moment(startDate).hours();
-                                let currentEndHour = moment(endDate).hours();
-
+                                let currentStartHour = moment(range.startDate).hours();
+                                let currentEndHour = moment(range.endDate).hours();
+                                
                                 Promise.each(events, (event) => {
                                     let checkStartHours = (currentStartHour+1 < event.startHour || currentStartHour+1 > event.startHour);
                                     let checkStartEndHours = (currentStartHour+1 < event.endHour || currentStartHour-1 > event.endHour);
@@ -180,7 +182,9 @@ exports.registerRoutes = function(app, config) {
                                     else{
                                         unavailable_dates.push(range);
                                     }
+                                    console.log(event);
                                 });
+                                
                             }
                             else{
                                 unavailable_dates.push(range);
@@ -188,7 +192,7 @@ exports.registerRoutes = function(app, config) {
                         });
                     });
                 }).then(() => {
-                    console.log(unavailable_dates);
+                    // console.log(unavailable_dates);
                     if(unavailable_dates.length === 0){
                         res.json({
                             message: 'Available Date'
