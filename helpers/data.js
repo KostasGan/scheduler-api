4e0511015a931c3f10c99e95870381e0691d5bc4
KostasGan <kostasgan@e-food.gr>
2018-06-18 17:14:13 +0300

    refactor and code style

---
+++ helpers/data.js
@@ -0,0 +1,28 @@
+const Promise = require('bluebird');
+
+exports.checkValidEmails = (attendees) => {
+    var re = /^(([^<>()\[\]\\.:@"]+(\.[^<>()\[\]\\.:@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let emails = attendees.trim().split(',');
+    let wrong_emails = 0;
+    return Promise.each(emails, (email) => {
+        if (!re.test(String(email.trim()).toLowerCase())) {
+            wrong_emails++;
+        }
+        return;
+    }).then(() => {
+        return Promise.resolve(wrong_emails);
+    })
+}
+
+exports.checkAvailableTime = (available_time) => {
+    var re = /^(?:(([01]?\d|2[0-3]):([0-5]?\d))-(([01]?\d|2[0-3]):([0-5]?\d)))$/;
+    let wrong_times = 0;
+    return Promise.each(available_time, (time) => {
+        if (!re.test(String(time.trim()))) {
+            wrong_times++;
+        }
+        return;
+    }).then(() => {
+        return Promise.resolve(wrong_times);
+    })
+}
--- routes/default.js
+++ routes/default.js
@@ -1,127 +1,71 @@
-const googleAuth = require('google-auth-library');
-const bodyParser = require('body-parser');
 const Promise = require('bluebird');
 const auth = require('../helpers/oauth');
+const date_helper = require('../helpers/date');
+const data_helper = require('../helpers/data');
 const ev_controler = require('../controllers/event-scheduler');
 const userModel = require('../models/user').Model;
 const moment = require('moment');
 
-
-function initGoogleAuth(credentials){
-    let auth = new googleAuth();
-
-    let googleOauth2Client = new auth.OAuth2(credentials.client_id, credentials.client_secret, credentials.redirect_uris);
-    // googleOauth2Client.credentials = {"access_token": ac_token};
-
-    return googleOauth2Client;
-}
-
-function checkValidEmails(attendees){
-    var re = /^(([^<>()\[\]\\.:@"]+(\.[^<>()\[\]\\.:@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let emails = attendees.trim().split(",");
-    let wrong_emails = 0;
-    return Promise.each(emails,(email) => {
-        if(!re.test(String(email.trim()).toLowerCase())){
-            wrong_emails++;
-        }
-        return;
-    }).then(() => {
-        return Promise.resolve(wrong_emails);
-    });
-}
-
-function checkAvailableTime(available_time){
-    var re = /^(?:(([01]?\d|2[0-3]):([0-5]?\d))-(([01]?\d|2[0-3]):([0-5]?\d)))$/;
-    let wrong_times = 0;
-    return Promise.each(available_time,(time) => {
-        if(!re.test(String(time.trim()))){
-            wrong_times++;
-        }
-        return;
-    }).then(() => {
-        return Promise.resolve(wrong_times);
-    });
-}
-
-function formatDateWithTime(startDate, diffDate, available_time){
-    let range = [];
-
-    for(let i=0; i <= diffDate; i++){
-        let av_time = available_time[i].trim().split(/[":\-"]/);
-        let new_date = new moment(startDate.toISOString());
-
-        if(av_time[i] !== "0" && av_time[i+1] !== "0"){
-            range.push({
-                "startDate": new_date.add(i, "d").set({'hour': av_time[0], 'minutes': av_time[1]}).toISOString(),
-                "endDate": new_date.set({'hour': av_time[2], 'minutes': av_time[3]}).toISOString()
-            });
-        }
-    }
-    return Promise.all(range);
-}
-
-exports.registerRoutes = function(app, config) {
+exports.registerRoutes = function (app, config) {
     let access_token;
-    let credentials = config.get("web");
-    
-    app.post("/", (req, res) => {
+
+    app.post('/', (req, res) => {
         let body = req.body;
         let startDate = moment(body.event_start) || moment().toISOString();
-        let endDate = moment(body.event_end) || moment().add(1, "h").toISOString();
+        let endDate = moment(body.event_end) || moment().add(1, 'h').toISOString();
         let duration = parseInt(body.event_duration, 10) || 1;
-        let available_time = body.available_time.split(",") || "";
-        let attendees = body.attendees || ""  ;
-        access_token = req.get('X-Access-Token') || "";
-        
-        let diffDate = endDate.diff(startDate,"day");
-        let dateRange = [];
+        let available_time = body.available_time.split(',') || '';
+        let attendees = body.attendees || '';
+        access_token = req.get('X-Access-Token').trim() || '';
 
-        if(access_token === "" || startDate === "" || endDate === "" || attendees === ""){
+        let diffDate = endDate.diff(startDate, 'day');
+
+        if (access_token === '' || startDate === '' || endDate === '' || attendees === '') {
             res.json({
-                status: "error",
-                message: "Bad Request. Please try again!"
+                status: 'error',
+                message: 'Bad Request. Please try again!'
             });
-            return; 
+            return;
         }
-        
-        if(available_time.length > diffDate){
-            checkAvailableTime(available_time).then((val) => { 
-                if (val > 0){
+
+        if (available_time.length > diffDate) {
+            data_helper.checkAvailableTime(available_time).then((val) => {
+                if (val > 0) {
                     res.json({
-                        status: "error",
-                        message: "Invalid Available Time. Try again!"
+                        status: 'error',
+                        message: 'Invalid Available Time. Try again!'
                     });
                     return;
                 }
             });
         }
-        else{
+        else {
             res.json({
-                status: "error",
-                message: "Available Time is less than Event Date range.  Try again!"
+                status: 'error',
+                message: 'Available Time is less than Event Date range.  Try again!'
             });
             return;
         }
 
-        checkValidEmails(attendees).then((val) => {
-            if (val > 0){
+        data_helper.checkValidEmails(attendees).then((val) => {
+            if (val > 0) {
                 res.json({
-                    status: "error",
-                    message: "Invalid Attendees emails. Try again!"
+                    status: 'error',
+                    message: 'Invalid Attendees emails. Try again!'
                 });
                 return;
             }
         });
 
-        let oauth2Client = initGoogleAuth(credentials);
+        let oauth2Client = auth.initGoogleAuth(config);
         let friends = userModel.findFriendsAccessToken(attendees);
         let main_user = userModel.findUserByAccessToken(access_token);
 
         friends.filter((friend) => {
-            oauth2Client.credentials = {"access_token": friend.ac_token};
+            oauth2Client.credentials = { 'access_token': friend.ac_token };
 
-            return auth.checkAuthToken(oauth2Client).then((val) =>{
-                if(val === "Invalid Credentials"){
+            return auth.checkAuthToken(oauth2Client).then((val) => {
+                if (val === 'Invalid Credentials') {
                     return friend.email;
                 }
                 return;
@@ -129,13 +73,13 @@ exports.registerRoutes = function(app, config) {
         }).map((friend) => {
             return friend.email;
         }).then((unAuthUsers) => {
-            if(unAuthUsers.length > 0){
+            if (unAuthUsers.length > 0) {
                 res.status(401);
                 res.json({
-                    status: "error",
-                    error_code: "unauthorized_friends",
-                    message: "Some of friendList's users need Authorization",
-                    data : {
+                    status: 'error',
+                    error_code: 'unauthorized_friends',
+                    message: 'Some of friendList`s users need Authorization',
+                    data: {
                         users: unAuthUsers
                     }
                 });
@@ -143,47 +87,47 @@ exports.registerRoutes = function(app, config) {
             }
 
             Promise.props({
-                main_user: main_user,        
+                main_user: main_user,
                 friends: friends
             }).then((props) => {
                 let users_tokens = [];
                 let unavailable_dates = [];
+                let dateRange = [];
 
-                formatDateWithTime(startDate, diffDate, available_time).then((v) => {
+                date_helper.formatDateWithTime(startDate, diffDate, available_time).then((v) => {
                     dateRange = v;
                 });
 
-                users_tokens.push(props.main_user ? props.main_user.ac_token : "");
-                
+                users_tokens.push(props.main_user ? props.main_user.ac_token : '');
+
                 props.friends.forEach((friend) => {
                     users_tokens.push(friend.ac_token);
                 });
 
                 return Promise.all(users_tokens).each((token) => {
-                    oauth2Client.credentials = {"access_token": token};
+                    oauth2Client.credentials = { 'access_token': token };
 
                     return Promise.each(dateRange, (range) => {
                         return ev_controler.GetCalendarEvents(oauth2Client, range.startDate, range.endDate).then((events) => {
-                            ev_controler.searchDateAvailability(events, range).then((va) =>{
-                                if(va.length > 0){
+                            ev_controler.searchDateAvailability(events, range).then((va) => {
+                                if (va.length > 0) {
                                     unavailable_dates = va;
                                 }
                             });
                         })
                     })
                 }).then(() => {
-                    // console.log(unavailable_dates);
-                    if(unavailable_dates.length === 0){
+                    if (unavailable_dates.length === 0) {
                         res.json({
-                            status: "success",
-                            message: "Available Date",
+                            status: 'success',
+                            message: 'Available Date',
                             data: unavailable_dates
                         });
                     }
-                    else{
+                    else {
                         res.json({
-                            status: "success",
-                            message: "Unavailable Date",
+                            status: 'success',
+                            message: 'Unavailable Date',
                             data: unavailable_dates
                         });
                     }
@@ -200,13 +144,15 @@ exports.registerRoutes = function(app, config) {
 
         access_token = req.body.access_token.trim() || '';
 
-        if(access_token === ''){
-            res.json({message: 'No access token. Please try again!'});
+        if (access_token === '') {
+            res.json({ message: 'No access token. Please try again!' });
             return;
         }
-        let oauth2Client = initGoogleAuth(credentials);
-        oauth2Client.credentials = {"access_token": access_token};
-        auth.authorizeClient(oauth2Client).then((val) =>{
+
+        let oauth2Client = auth.initGoogleAuth(config);
+        oauth2Client.credentials = { 'access_token': access_token };
+
+        auth.authorizeClient(oauth2Client).then((val) => {
             res.json(val);
         }).catch((error) => {
             res.status(401);
