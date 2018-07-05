78c6c72bb6a0bcb02b0ec7a8bcbcc91b31b54592
KostasGan <kostasgan@e-food.gr>
2018-07-05 16:59:28 +0300

    refactor to hadle main_user=null

--- routes/event-scheduler.js
+++ routes/event-scheduler.js
@@ -14,12 +14,11 @@ exports.registerRoutes = function (app, config) {
         let body = req.body;
         let startDate = moment(body.event_start) || moment().toISOString();
         let endDate = moment(body.event_end) || moment().add(1, 'h').toISOString();
+        let diffDate = endDate.diff(startDate, 'day');
         let duration = parseInt(body.event_duration, 10) || 1;
         let available_time = body.available_time ? body.available_time.split(',') : '';
         let attendees = body.attendees || '';
         access_token = req.get('X-Access-Token') ? req.get('X-Access-Token').trim() : '';
-
-        let diffDate = endDate.diff(startDate, 'day');
 
         if (access_token === '' || startDate === '' || endDate === '' || attendees === '') {
             res.json({
@@ -47,6 +46,7 @@ exports.registerRoutes = function (app, config) {
             return;
         }
 
+
         data_helper.checkValidEmails(attendees).then((val) => {
             if (val > 0) {
                 res.json({
@@ -55,79 +55,104 @@ exports.registerRoutes = function (app, config) {
                 });
                 return;
             }
-        });
-
-        let oauth2Client = auth.initGoogleAuth(config);
-        let all_users = join(userModel.findFriendsAccessToken(attendees), userModel.findUserByAccessToken(access_token), (attendees, main_user) => {
-            attendees.push(main_user);
-            return attendees;
-        });
-
-        all_users.filter((user) => {
-            oauth2Client.credentials = { 'access_token': user.ac_token };
-
-            return auth.checkAuthToken(oauth2Client).then((val) => {
-                if (val === 'Invalid Credentials') {
-                    return user.email;
-                }
-                return;
-            });
-        }).map((user) => {
-            return user.email;
-        }).then((unAuthUsers) => {
-            if (unAuthUsers.length > 0) {
-                res.status(401);
-                res.json({
-                    status: 'error',
-                    error_code: 'unauthorized_friends',
-                    message: 'Some of friendList`s users need Authorization',
-                    data: {
-                        users: unAuthUsers
-                    }
-                });
+            return {};
+        }).then((val) => {
+            if(!val){
                 return;
             }
 
-            let unavailable_dates = [];
-            let dateRange = date_helper.formatDateWithTime(startDate, diffDate, available_time);
-
-            return Promise.each(all_users, (token) => {
-                oauth2Client.credentials = { 'access_token': token.ac_token };
-
-                return Promise.each(dateRange, (range) => {
-                    return ev_controler.GetCalendarEvents(oauth2Client, range.startDate, range.endDate)
-                        .then((events) => {
-                            return ev_controler.searchDateAvailability(events.events, range)
-                        })
-                        .each((dt) => {
-                            if (!unavailable_dates.includes(dt)) {
-                                unavailable_dates.push(dt);
-                            }
-                            return;
-                        });
-                });
-            }).then(() => {
-                if (unavailable_dates.length === 0) {
-                    res.json({
-                        status: 'success',
-                        message: 'Available Date',
-                        data: unavailable_dates
-                    });
-                } else {
-                    res.json({
-                        status: 'success',
-                        message: 'Unavailable Date',
-                        data: unavailable_dates
-                    });
+            let oauth2Client = auth.initGoogleAuth(config);
+            let all_users = join(userModel.findFriendsAccessToken(attendees), userModel.findUserByAccessToken(access_token), (attendees, main_user) => {
+                if(main_user && attendees){
+                    attendees.push(main_user);
+                    return attendees;
                 }
+
+                res.status(401);
+                res.json({
+                    status: 'error',
+                    error_code: 'main_user_not_found',
+                    message: 'No user found with access token!',
+                    data: {}
+                });
+                return [];
             });
-        }).catch((e) => {
-            console.log(e);
-            res.json({
-                status: 'error',
-                message: 'Bad Request. Please try again!'
+
+            Promise.all(all_users).filter((user) => {
+                oauth2Client.credentials = { 'access_token': user.ac_token };
+                return auth.checkAuthToken(oauth2Client).then((val) => {
+                    if (val === 'Invalid Credentials') {
+                        return user.email;
+                    }
+                    return;
+                });
+            })
+            .map((user) => {
+                return user.email;
+            })
+            .then((unAuthUsers) => {
+                if (unAuthUsers.length > 0) {
+                    res.status(401);
+                    res.json({
+                        status: 'error',
+                        error_code: 'unauthorized_friends',
+                        message: 'Some of friendList`s users need Authorization',
+                        data: {
+                            users: unAuthUsers
+                        }
+                    });
+                    return;
+                }
+                return all_users;
+            })
+            .then((all_users) => {
+                if(!all_users || all_users.length === 0){
+                    return;
+                }
+
+                let unavailable_dates = [];
+                let dateRange = date_helper.formatDateWithTime(startDate, diffDate, available_time);
+
+                return Promise.each(all_users, (token) => {
+                    oauth2Client.credentials = { 'access_token': token.ac_token };
+
+                    return Promise.each(dateRange, (range) => {
+                        return ev_controler.GetCalendarEvents(oauth2Client, range.startDate, range.endDate)
+                            .then((events) => {
+                                return ev_controler.searchDateAvailability(events.events, range)
+                            })
+                            .each((dt) => {
+                                if (!unavailable_dates.includes(dt)) {
+                                    unavailable_dates.push(dt);
+                                }
+                                return;
+                            });
+                    });
+                })
+                .then(() => {
+                    if (unavailable_dates.length === 0) {
+                        res.json({
+                            status: 'success',
+                            message: 'Available Date',
+                            data: unavailable_dates
+                        });
+                    } else {
+                        res.json({
+                            status: 'success',
+                            message: 'Unavailable Date',
+                            data: unavailable_dates
+                        });
+                    }
+                });
+            })
+            .catch((e) => {
+                console.log(e);
+                res.json({
+                    status: 'error',
+                    message: 'Bad Request. Please try again!'
+                });
+                return;
             });
-            return;
         });
     });
 }
