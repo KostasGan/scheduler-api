e688e930941da12e4ae30d1b5c5109b9c1760281
Kostas Gan <kostasgan@e-food.gr>
2018-07-11 18:58:30 +0300

    last commit plzzz

--- helpers/date.js
+++ helpers/date.js
@@ -3,12 +3,12 @@ const moment = require('moment-timezone');
 
 exports.formatDateWithTime = (startDate, diffDate, available_time) => {
     let range = [];
-    let timezone = 'America/New_York'
+    let timezone = 'Europe/Athens'
     for (let i = 0; i <= diffDate; i++) {
         let av_time = available_time[i].trim().split(/[":\-"]/);
-        let newISOStartDate = startDate.toISOString();
-        let new_date = new moment(newISOStartDate).add(i, 'd');
-        let new_enddate = new moment(newISOStartDate).add(i, 'd');
+        let newISOStartDate = startDate.tz(timezone).toISOString();
+        let new_date = new moment(newISOStartDate).tz(timezone).add(i, 'd');
+        let new_enddate = new moment(newISOStartDate).tz(timezone).add(i, 'd');
 
         console.log(new_date.format());
         if (av_time[i] !== '0' && av_time[i + 1] !== '0') {
