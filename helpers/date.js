0cb8983e7641ccdb9fdeaa105a103d819aece1c9
KostasGan <kostasgan@e-food.gr>
2018-07-11 14:22:23 +0300

    refact

--- helpers/date.js
+++ helpers/date.js
@@ -6,7 +6,8 @@ exports.formatDateWithTime = (startDate, diffDate, available_time) => {
 
     for (let i = 0; i <= diffDate; i++) {
         let av_time = available_time[i].trim().split(/[":\-"]/);
-        let new_date = new moment(startDate.toISOString());
+        let newStartDate = startDate.toISOString();
+        let new_date = new moment(newStartDate);
         console.log(new_date);
         if (av_time[i] !== '0' && av_time[i + 1] !== '0') {
             range.push({
