fb8556fab7d07e8760754b76bccacb0171849437
KostasGan <kostasgan@e-food.gr>
2018-07-11 17:25:47 +0300

    revert for now

--- helpers/date.js
+++ helpers/date.js
@@ -1,9 +1,9 @@
 const Promise = require('bluebird');
-const moment = require('moment-timezone');
+const moment = require('moment');
 
 exports.formatDateWithTime = (startDate, diffDate, available_time) => {
     let range = [];
-    let timezone = 'Europe/Athens';
+
     for (let i = 0; i <= diffDate; i++) {
         let av_time = available_time[i].trim().split(/[":\-"]/);
         let newISOStartDate = startDate.toISOString();
@@ -13,15 +13,11 @@ exports.formatDateWithTime = (startDate, diffDate, available_time) => {
         if (av_time[i] !== '0' && av_time[i + 1] !== '0') {
             new_date.set({ 'hour': av_time[0], 'minutes': av_time[1] });
             new_enddate.set({ 'hour': av_time[2], 'minutes': av_time[3] })
-            console.log(new moment().format());
-            let ss = new_date.tz(timezone).toISOString();
-            let sss = new_enddate.tz(timezone).toISOString();
             range.push({
-                'startDate': ss,
-                'endDate': sss
+                'startDate': new_date.toISOString(),
+                'endDate': new_enddate.toISOString()
             });
         }
     }
-    console.log(range)
     return Promise.all(range);
 }
