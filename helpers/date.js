444f030e739f5c2a5b17b499c448425abb9a5d30
KostasGan <kostasgan@e-food.gr>
2018-07-11 17:02:37 +0300

    another one

--- helpers/date.js
+++ helpers/date.js
@@ -13,8 +13,8 @@ exports.formatDateWithTime = (startDate, diffDate, available_time) => {
         if (av_time[i] !== '0' && av_time[i + 1] !== '0') {
             new_date.set({ 'hour': av_time[0], 'minutes': av_time[1] });
             new_enddate.set({ 'hour': av_time[2], 'minutes': av_time[3] })
-            console.log(av_time[0] + " " + av_time[1])
-            console.log(av_time[2] + " " + av_time[3])
+            console.log(new_date)
+            console.log(new_enddate)
             let ss = new_date.toISOString();
             let sss = new_enddate.toISOString();
             range.push({
