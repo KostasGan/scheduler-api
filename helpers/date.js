5f98ab72e88971f46f67f3cc263d9fa03def5ea3
Kostas Gan <kostasgan@e-food.gr>
2018-07-11 18:48:28 +0300

    ppff

--- helpers/date.js
+++ helpers/date.js
@@ -10,13 +10,16 @@ exports.formatDateWithTime = (startDate, diffDate, available_time) => {
         let new_date = new moment(newISOStartDate).add(i, 'd');
         let new_enddate = new moment(newISOStartDate).add(i, 'd');
 
+        console.log(new_date.format());
         if (av_time[i] !== '0' && av_time[i + 1] !== '0') {
             new_date.set({ 'hour': av_time[0], 'minutes': av_time[1] });
-            new_enddate.set({ 'hour': av_time[2], 'minutes': av_time[3] })
+            new_enddate.set({ 'hour': av_time[2], 'minutes': av_time[3] });
+            console.log(new_date.format());
             range.push({
                 'startDate': new_date.toISOString(),
                 'endDate': new_enddate.toISOString()
             });
+            console.log(new_date.toISOString());
         }
     }
     return Promise.all(range);
