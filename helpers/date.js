c1df3d1c58979b044b1a5b512b77c69a7ae308b1
KostasGan <kostasgan@e-food.gr>
2018-07-11 15:32:33 +0300

    possible fix

--- helpers/date.js
+++ helpers/date.js
@@ -6,15 +6,19 @@ exports.formatDateWithTime = (startDate, diffDate, available_time) => {
 
     for (let i = 0; i <= diffDate; i++) {
         let av_time = available_time[i].trim().split(/[":\-"]/);
-        let newStartDate = startDate.toISOString();
-        let new_date = new moment(newStartDate);
+        let newISOStartDate = startDate.toISOString();
+        let new_date = new moment(newISOStartDate).add(i, 'd');
+        let new_enddate = new moment(newISOStartDate).add(i, 'd');
 
         if (av_time[i] !== '0' && av_time[i + 1] !== '0') {
+            console.log(av_time[0] + " " + av_time[1])
+            console.log(av_time[2] + " " + av_time[3])
             range.push({
-                'startDate': new_date.add(i, 'd').set({ 'hour': av_time[0], 'minutes': av_time[1] }).toISOString(),
-                'endDate': new_date.set({ 'hour': av_time[2], 'minutes': av_time[3] }).toISOString()
+                'startDate': new_date.set({ 'hour': av_time[0], 'minutes': av_time[1] }).utc().toISOString(),
+                'endDate': new_enddate.set({ 'hour': av_time[2], 'minutes': av_time[3] }).utc().toISOString()
             });
         }
     }
+    console.log(range)
     return Promise.all(range);
 }
